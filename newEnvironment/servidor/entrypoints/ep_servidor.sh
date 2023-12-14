b4c95e84bd750c0d6e77ea5a961dbb9af2f92388
Miguel Angel Puentes Vallejo <ma.puentes@alumnos.upm.es>
2023-12-14 20:55:14 +0100

    mi proyecto actualizado

--- newEnvironment/servidor/entrypoints/ep_servidor.sh
+++ newEnvironment/servidor/entrypoints/ep_servidor.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-ip route change default via 10.100.0.1
+
 apt-get update
 apt-get install -y node.js
 apt-get install -y npm
+
+#COPY entrypoints/SSR-master-server-main /SSR-master-server-main
 git clone https://github.com/gisai/SSR-master-server.git
 cd SSR-master-server
 npm install -g nodemon@1.18.10
 npm install
+ip route change default via 10.100.0.10
 npm start
+
+
