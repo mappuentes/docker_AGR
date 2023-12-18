817eb072f182be73ec6a3cbe68324c66fe0d3f34
Miguel Angel Puentes Vallejo <ma.puentes@alumnos.upm.es>
2023-12-18 20:50:47 +0100

    mi proyecto actualizado

--- newEnvironment/servidor/entrypoints/ep_servidor.sh
+++ newEnvironment/servidor/entrypoints/ep_servidor.sh
@@ -5,7 +5,7 @@ apt-get install -y node.js
 apt-get install -y npm
 
 #COPY entrypoints/SSR-master-server-main /SSR-master-server-main
-git clone https://github.com/gisai/SSR-master-server.git
+git clone https://github.com/mappuentes/SSR-master-server.git
 cd SSR-master-server
 npm install -g nodemon@1.18.10
 npm install
